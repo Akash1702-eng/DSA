9522ee63a19a8d8a5ec4d7a560f7d77477236a8d
Akash Khurd <akashkhurd1702@gmail.com>
2026-06-23 14:45:18 +0530

    Add: Search a 2D Matrix (Day 7)

---
+++ Day 7/search-a-2d-matrix.cpp
@@ -0,0 +1,25 @@
+// Problem: Search a 2D Matrix
+// Difficulty: Medium
+// Status: Accepted
+// Submitted: June 23, 2026
+// URL: https://leetcode.com/problems/search-a-2d-matrix/submissions/2043177935/
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int low = 0, high = n * m - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int row = mid / m;
+            int col = mid % m;
+
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] < target)
+                low = mid +
