1ef4b9b67fb8b28a591c629ee2f9ecb991b6e73f
Akash Khurd <akashkhurd1702@gmail.com>
2026-06-24 23:35:55 +0530

    Add: Majority Element II (Day 8)

---
+++ Day 8/majority-element-ii.cpp
@@ -0,0 +1,45 @@
+// Problem: Majority Element II
+// Difficulty: Medium
+// Status: Accepted
+// Submitted: June 24, 2026
+// URL: https://leetcode.com/problems/majority-element-ii/submissions/2044953161/
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int cnt1 = 0, cnt2 =  0;
+        int el1 = INT_MIN, el2 = INT_MIN;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(cnt1 == 0 && el2 != nums[i])
+            {
+                cnt1 = 1;
+                el1 = nums[i];
+            }
+            else if(cnt2 == 0 && el1 != nums[i])
+            {
+                cnt2 = 1;
+                el2 = nums[i];
+            }
+            else if(el1 == nums[i]) cnt1++;
+            else if(el2 == nums[i]) cnt2++;
+            else 
+            {
+                cnt1--, cnt2--;
+            }
+        }
+        vector<int> ls;
+        cnt1 = 0, cnt2 = 0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i] == el1) cnt1++;
+            if(nums[i] == el2) cnt2++;
+        }
+
+        int mini = (int)(nums.size()/3)+1;
+
+        if(cnt1 >= mini) ls.push_back(el1);
+        if(cnt2 >= mini) ls.push_back(el2);
+        sort(ls.begin(),ls.end());
+        return ls;
+    }
+};
