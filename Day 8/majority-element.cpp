856abd3874348ec33a2210694c7ef58ac4052671
Akash Khurd <akashkhurd1702@gmail.com>
2026-06-24 15:25:30 +0530

    Add: Majority Element (Day 8)

---
+++ Day 8/majority-element.cpp
@@ -0,0 +1,24 @@
+// Problem: Majority Element
+// Difficulty: Easy
+// Status: Unknown
+// Submitted: June 24, 2026
+// URL: https://leetcode.com/problems/majority-element/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> mpp;
+        for(int i=0; i<nums.size(); i++)
+        {
+            mpp(v[i])++;
+        }
+
+        for(auto it:mpp)
+        {
+            if(it.second > (nums.size()/2))
+            {
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
