0a4c61e7b8c029fcbae66791eaa2186d37460476
Akash Khurd <akashkhurd1702@gmail.com>
2026-06-25 17:22:32 +0530

    Add: Reverse Pairs (Day 9)

--- Day 9/reverse-pairs.cpp
+++ Day 9/reverse-pairs.cpp
@@ -2,23 +2,76 @@
 // Difficulty: Hard
 // Status: Accepted
 // Submitted: June 25, 2026
-// URL: https://leetcode.com/problems/reverse-pairs/submissions/2045748091/
+// URL: https://leetcode.com/problems/reverse-pairs/submissions/2045750555/
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        } else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+int countPairs(vector<int> &arr, int low, int mid, int high) {
+    int right = mid + 1;
+    int cnt = 0;
+
+    for (int i = low; i <= mid; i++) {
+        while (right <= high && (long long)arr[i] > 2LL * arr[right]) {
+            right++;
+        }
+        cnt += (right - (mid + 1));
+    }
+
+    return cnt;
+}
+
+int mergeSort(vector<int> &arr, int low, int high) {
+    int cnt = 0;
+
+    if (low >= high) return cnt;
+
+    int mid = (low + high) / 2;
+
+    cnt += mergeSort(arr, low, mid);
+    cnt += mergeSort(arr, mid + 1, high);
+    cnt += countPairs(arr, low, mid, high);
+    merge(arr, low, mid, high);
+
+    return cnt;
+}
+
+int team(vector<int> &skill, int n) {
+    return mergeSort(skill, 0, n - 1);
+}
+
 class Solution {
 public:
     int reversePairs(vector<int>& nums) {
-        int cnt = 0;
-
-        for(int i = 0; i < nums.size(); i++)
-        {
-            for(int j = i + 1; j < nums.size(); j++)
-            {
-                if((long long)nums[i] > 2LL * nums[j])
-                {
-                    cnt++;
-                }
-            }
-        }
-
-        return cnt;
+        return team(nums, nums.size());
     }
 };
