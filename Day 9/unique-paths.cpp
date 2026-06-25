7d239ad466057c48020dede2f7c1d46224a47388
Akash Khurd <akashkhurd1702@gmail.com>
2026-06-25 15:28:49 +0530

    Add: Unique Paths (Day 9)

--- Day 9/unique-paths.cpp
+++ Day 9/unique-paths.cpp
@@ -2,18 +2,18 @@
 // Difficulty: Medium
 // Status: Accepted
 // Submitted: June 25, 2026
-// URL: https://leetcode.com/problems/unique-paths/submissions/2045631065/
+// URL: https://leetcode.com/problems/unique-paths/submissions/2045640555/
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        int N = n + m - 2;
-        int r = m - 1;
-        double res = 1;
-
-        for(int i = 1; i <= r; i++)
-        {
-            res = res*(N-r+i)/i;
-        }
-        return (int)res;
+    int f(int m, int n) {
+        if(m == 0 && n == 0) return 1;
+        if(m < 0 || n < 0) return 0;
+        int up = f(m-1, n);
+        int left = f(m, n-1);
+        return up+left; 
+    }
+    int uniquePaths(int i, int j)
+    {
+        return f(i-1, j-1);
     }
 };
