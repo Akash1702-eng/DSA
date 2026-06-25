1d4297a1bb39f00bdc9cd997b683df03e5979c05
Akash Khurd <akashkhurd1702@gmail.com>
2026-06-25 17:15:28 +0530

    Add: Reverse Pairs (Day 9)

--- Day 9/reverse-pairs.cpp
+++ Day 9/reverse-pairs.cpp
@@ -2,14 +2,14 @@
 // Difficulty: Hard
 // Status: Accepted
 // Submitted: June 25, 2026
-// URL: https://leetcode.com/problems/reverse-pairs/submissions/2045743880/
+// URL: https://leetcode.com/problems/reverse-pairs/submissions/2045744864/
 class Solution {
 public:
     int reversePairs(vector<int>& nums) {
         int cnt = 0;
         for(int i = 0; i < nums.size(); i++)
         {
-            for(int j = 1; j < nums.size(); j++)
+            for(int j = i + 1; j < nums.size(); j++)
             {
                 if(nums[i] > (nums[j] * 2))
                 {
