c769f5effa762e34103821983dd26d7676bee410
Akash Khurd <akashkhurd1702@gmail.com>
2026-07-01 23:20:57 +0530

    Add: Merge Two Sorted Lists (Day 15)

---
+++ Day 15/merge-two-sorted-lists.cpp
@@ -0,0 +1,40 @@
+// Problem: Merge Two Sorted Lists
+// Difficulty: Easy
+// Status: Accepted
+// Submitted: July 1, 2026
+// URL: https://leetcode.com/problems/merge-two-sorted-lists/submissions/2052677238/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy;
+        ListNode* tail = &dummy;
+
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val <= list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            } else {
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next;
+        }
+
+        if (list1 != nullptr)
+            tail->next = list1;
+        else
+            tail->next = list2;
+
+        return dummy.next;
+    }
+};
