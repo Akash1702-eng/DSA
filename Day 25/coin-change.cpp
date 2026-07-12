b6cae1233baf568f3917f6d46e586da447e65212
Akash Khurd <akashkhurd1702@gmail.com>
2026-07-12 23:43:14 +0530

    Add: Coin Change (Day 25)

---
+++ Day 25/coin-change.cpp
@@ -0,0 +1,22 @@
+// Problem: Coin Change
+// Difficulty: Medium
+// Status: Accepted
+// Submitted: July 12, 2026
+// URL: https://leetcode.com/problems/coin-change/submissions/2065328436/
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+};
