311730b1a509f3570afbcb922c668007c7b1d8f3
Akash Khurd <akashkhurd1702@gmail.com>
2026-07-16 19:43:16 +0530

    Add: Max Consecutive Ones (Day 28)

---
+++ Day 28/max-consecutive-ones.cpp
@@ -0,0 +1,24 @@
+// Problem: Max Consecutive Ones
+// Difficulty: Easy
+// Status: Accepted
+// Submitted: July 16, 2026
+// URL: https://leetcode.com/problems/max-consecutive-ones/submissions/2069958231/
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt = 0, maxi = 0;
+        for(int i = 0; i<nums.size(); i++)
+        {
+            if(nums[i] == 1)
+            {
+                cnt++;
+                maxi = max(cnt, maxi);
+            }
+            else
+            {
+                cnt = 0;
+            }
+        }
+        return maxi;
+    }
+};
