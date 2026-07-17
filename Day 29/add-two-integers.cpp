1d7b1adc49eabd6e01fe62eee8a5bbed5e07b06b
Akash Khurd <akashkhurd1702@gmail.com>
2026-07-17 22:10:05 +0530

    Add: Add Two Integers (Day 29)

---
+++ Day 29/add-two-integers.cpp
@@ -0,0 +1,11 @@
+// Problem: Add Two Integers
+// Difficulty: Easy
+// Status: Accepted
+// Submitted: July 17, 2026
+// URL: https://leetcode.com/problems/add-two-integers/submissions/2071349589/?submissionId=2071305906
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;  
+    }
+};
