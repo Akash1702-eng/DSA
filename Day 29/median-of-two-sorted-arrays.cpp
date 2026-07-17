4c62e4cb1c556d94afd0b7a65122e922267536cc
Akash Khurd <akashkhurd1702@gmail.com>
2026-07-17 22:11:10 +0530

    Add: Median of Two Sorted Arrays (Day 29)

---
+++ Day 29/median-of-two-sorted-arrays.cpp
@@ -0,0 +1,21 @@
+// Problem: Median of Two Sorted Arrays
+// Difficulty: Hard
+// Status: Accepted
+// Submitted: July 17, 2026
+// URL: https://leetcode.com/problems/median-of-two-sorted-arrays/submissions/2071350755/?submissionId=2071327736
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+
+        sort(nums1.begin(), nums1.end());
+
+        int n = nums1.size();
+
+        if (n % 2 == 0) {
+            return (nums1[n/2 - 1] + nums1[n/2]) / 2.0;
+        } else {
+            return nums1[n/2];
+        }
+    }
+};
